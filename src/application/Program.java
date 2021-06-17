fd847278eddb18525bfbc41ea96648ec5974efba
Rodrigo Gustavo <2338.gustavo@gmail.com>
2021-06-16 21:10:37 -0300

    aula 186

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,7 +1,9 @@
 package application;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 
 public class Program {
@@ -21,6 +23,20 @@ public class Program {
 		}
 		catch (IOException e) {
 			System.out.println("Error: " + e.getMessage());
+		}
+		
+		String[] lines = new String[] { "Testando escrita em arquivos", "Testando sintaxe do desenvolvimento em java", "Testando se o texto digitado aqui realmente será criado em um novo arquivo"};
+		
+		String path1 = "c:\\Projetos Java\\out.txt";
+		
+		try (BufferedWriter bw = new BufferedWriter(new FileWriter(path1))) {
+			for (String line : lines) {
+				bw.write(line);
+				bw.newLine();
+			}
+		}
+		catch (IOException e) {
+			e.printStackTrace();
 		}
 
 	}
